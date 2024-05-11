24294a6350ed16fcb232dfeba255253d93611563
chenhh116 <chenhh116@chinaunicom.cn>
2024-05-11 15:13:19 +0800

    [Add]add manager

---
+++ CCWebDAVDemo/CCWebDAVDemo/CCWebDavManager.h
@@ -0,0 +1,16 @@
+//
+//  CCWebDavManager.h
+//  CCWebDAVDemo
+//
+//  Created by Carven on 2024/5/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCWebDavManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCWebDAVDemo/CCWebDAVDemo/CCWebDavManager.m
@@ -0,0 +1,12 @@
+//
+//  CCWebDavManager.m
+//  CCWebDAVDemo
+//
+//  Created by Carven on 2024/5/11.
+//
+
+#import "CCWebDavManager.h"
+
+@implementation CCWebDavManager
+
+@end
